0e21693623df4bb1ec0e4344fa22b33d534d0331
yusuf Demir <yusuf.ferrum@gmail.com>
2022-10-08 11:06:49 +0200

    feedbacks applied

--- homework/hackyourtempature/server.js
+++ homework/hackyourtempature/server.js
@@ -1,17 +1,19 @@
 import express from "express";
 const app = express();
-
 const port = 3000;
+
+app.use(express.json());
 app.use(express.urlencoded());
 app.get("/", (req, res) => {
   res.send(`
+    <h3>Hello from backend to frontend</h3>
     <form action="/weather" method="POST">
         <input type="text" name="city" ></input>
         <input type="submit"></input>
     </form>
     `);
 });
-app.use(express.json());
+
 app.post("/weather", (req, res, next) => {
   const cityName = req.body.city;
   res.send(cityName);
